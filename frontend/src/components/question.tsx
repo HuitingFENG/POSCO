41e5025f74064efae426c74a42a9ec10b35216c6
FENG <38035703+HuitingFENG@users.noreply.github.com>
2024-01-13 10:08:55 +0100

    update

--- frontend/src/components/question.tsx
+++ frontend/src/components/question.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 import { Box,Flex,Link,Text,Image,Button,Stack,Center,Icon } from "@chakra-ui/react";
+import { useState, useEffect } from 'react';
+
+interface Question {
+    question_text: string;
+    // ... other properties ...
+}
 
 const Question = () => {
+    const [questions, setQuestions] = useState<Question[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/questions')
+            .then(response => response.json())
+            .then(data => {
+                setQuestions(data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <Flex justify="space-between" align="center" flexDirection="column" gap={10}>
             <Flex flex="2" border="2px" borderColor="black" p={10} bgColor="tomato">
@@ -10,6 +36,14 @@ const Question = () => {
 
             <Flex flex="3" border="2px" borderColor="black" p={10} bgColor="skyblue">
                 Vos Réponses :
+                <div>
+                    <h1>Questions</h1>
+                    <ul>
+                        {questions.map((question, index) => (
+                        <li key={index}>{question.question_text}</li>
+                        ))}
+                    </ul>
+                </div>
             </Flex>
         </Flex>
     );
