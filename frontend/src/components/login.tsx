b33707723228f89624a8d633d246244f2331ea2f
FENG <38035703+HuitingFENG@users.noreply.github.com>
2024-01-14 23:55:40 +0100

    update

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,61 +1,14 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Box, Button, Input, VStack, useToast } from '@chakra-ui/react';
-import { FaQuestionCircle, FaBook, FaCog, FaUser, FaChartBar, FaRegLightbulb, FaUserPlus, FaSignInAlt } from "react-icons/fa";
-
+import { FaSignInAlt } from "react-icons/fa";
 
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const toast = useToast();
-
-/*     const handleSubmit = async () => {
-        try {
-            const response = await fetch('http://localhost:3001/users/login', { // Make sure the URL matches your server's configuration
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
-
-
-            const data = await response.json();
-
-            if (response.ok) {
-                toast({
-                    title: 'Login successful.',
-                    description: data.message,
-                    status: 'success',
-                    duration: 5000,
-                    isClosable: true,
-                });
-                
-                // window.location.href = 'http://localhost:3000/profil';
-            } else {
-                throw new Error(data.message || 'Failed to login');
-            }
-        } catch (error) {
-            if (error instanceof Error) {
-                toast({
-                    title: 'Login failed.',
-                    description: error.message,
-                    status: 'error',
-                    duration: 5000,
-                    isClosable: true,
-                });
-            } else {
-                // Handle cases where the error is not an instance of Error
-                toast({
-                    title: 'Login failed.',
-                    description: 'An unknown error occurred',
-                    status: 'error',
-                    duration: 5000,
-                    isClosable: true,
-                });
-            }
-        }
-    }; */
+    const navigate = useNavigate();
 
     const sendUser = () => {
         console.log("TEST email: ", email);
@@ -86,8 +39,8 @@ const Login = () => {
             setEmail('');
             setPassword('');
             setTimeout(() => {
-                window.location.href = 'http://localhost:3000/profil';
-            }, 5000);
+                navigate('/profil'); 
+            }, 1000);
         })
         .catch((error) => {
             console.error('Error:', error);
@@ -124,39 +77,6 @@ const Login = () => {
             </Button>
         </VStack>
     );
-
-
-/*     const handleSubmit = async () => {
-        
-        toast({
-            title: 'Login successful.',
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-        });
-    };
-
-    return (
-        <VStack spacing={4}>
-            <Input 
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input 
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button colorScheme="blue" onClick={handleSubmit}>Se Connecter</Button>
-        </VStack>
-    ); */
-
-
-
-
-
 
 };
 
--- frontend/src/components/signup.tsx
+++ frontend/src/components/signup.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Box, Button, Input, VStack, useToast } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Input, VStack, useToast } from '@chakra-ui/react';
+
 
 const Signup = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const toast = useToast();
-    // const [input, setInput] = useState<string[]>([]);
-
+    const navigate = useNavigate();
 
     const sendUser = () => {
         fetch('http://localhost:3001/api/users/signup', {
@@ -36,8 +37,8 @@ const Signup = () => {
             setEmail('');
             setPassword('');
             setTimeout(() => {
-                window.location.href = 'http://localhost:3000/login';
-            }, 5000);
+                navigate('/login'); 
+            }, 1000);
         })
         .catch((error) => {
             console.error('Error:', error);
@@ -55,36 +56,6 @@ const Signup = () => {
         e.preventDefault(); 
         sendUser();
     };
-
-/*     const sendResponses = () => {
-        // Validation before sending responses
-        if (!validateResponses()) {
-            // If validation fails, exit the function
-            return;
-        }
-    
-        const formattedResponses = responses.map((answer, index) => ({
-            userId: 1, 
-            questionId: questions[index].id, 
-            answer: answer
-        }));
-    
-        // ... [existing fetch request code] ...
-    };
-    
-    const validateResponses = () => {
-        // Basic validation example
-        for (let i = 0; i < questions.length; i++) {
-            const response = responses[i];
-            if (!response || response.trim() === '') {
-                // Trigger an error message for empty response
-                alert(`Response for question ${i + 1} is required.`);
-                return false;
-            }
-            // Add more validation logic here if needed
-        }
-        return true;
-    }; */
 
     return (
         <VStack spacing={4} as="form" onSubmit={handleSubmit}>
@@ -107,41 +78,6 @@ const Signup = () => {
             <Button colorScheme="blue" type="submit">S'inscrire</Button>
         </VStack>
     );
-
-
-
-/* 
-
-    const handleSubmit = async () => {
-        toast({
-            title: 'Signup successful.',
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-        });
-    };
-
-    return (
-        <VStack spacing={4}>
-            <Input 
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <Input 
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input 
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button colorScheme="blue" onClick={handleSubmit}>Sign Up</Button>
-        </VStack>
-    ); */
 };
 
 export default Signup;
